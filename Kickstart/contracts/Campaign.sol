b83452ab8854813f74df9f17fbfa27919b0d943b
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-16 01:42:58 +0200

    Enable creating a request for the payment to a vendor

--- Kickstart/contracts/Campaign.sol
+++ Kickstart/contracts/Campaign.sol
@@ -1,9 +1,25 @@
 pragma solidity ^0.4.17;
 
 contract Campaign {
+    struct Request {
+        // contains reason why a request should be processed and the money sent to a vendor
+        string description;
+        // says how much money we ant to send to a vendor
+        uint value;
+        address recipient;
+        // says if the request is completed and the money sent to the vendor
+        bool isCompleted;
+    }
+    
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    Request[] public requestList;
+    
+    modifier restrictedToManager() {
+        require(msg.sender == manager);
+        _;
+    }
     
     constructor(uint minimumAmount) public {
         manager = msg.sender;
@@ -16,5 +32,17 @@ contract Campaign {
         require(msg.value >= minimumContribution);
         
         approvers.push(msg.sender);
+    }
+    
+    function createRequest(string description, uint value, address recipient) 
+        public restrictedToManager {
+        Request newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            isCompleted: false
+        });
+        
+        requestList.push(newRequest);
     }
 }
