faa8baae56be102fb9f33232abe06b425548c86e
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-16 00:50:15 +0200

    Enable to users to contribute to the campaign

--- Kickstart/contracts/Campaign.sol
+++ Kickstart/contracts/Campaign.sol
@@ -2,8 +2,19 @@ pragma solidity ^0.4.17;
 
 contract Campaign {
     address public manager;
+    uint public minimumContribution;
+    address[] public approvers;
     
-    constructor() {
+    constructor(uint minimumAmount) public {
         manager = msg.sender;
+        minimumContribution = minimumAmount;
+    }
+    
+    function contribute() public payable {
+        // msg.value contains amount of money a caller sent in the transaction
+        // in order to call this function
+        require(msg.value >= minimumContribution);
+        
+        approvers.push(msg.sender);
     }
 }
