2f745415f94153e158bed2b3fffa928ec5cdfd15
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-15 22:00:57 +0200

    Change single quotes to double quotes in Angular

--- Lottery/frontend-angular/src/app/app.component.ts
+++ Lottery/frontend-angular/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { web3 } from "../web3";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'frontend-angular';
+  title = "frontend-angular";
+
+  constructor() {
+    console.log(web3.version);
+  }
 }
