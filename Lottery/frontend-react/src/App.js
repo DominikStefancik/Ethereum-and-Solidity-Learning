dd7ab5df137d2be866c0746486c704e2ff2dec77
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-15 13:33:08 +0200

    Add a form to enter the lottery

--- Lottery/frontend-react/src/App.js
+++ Lottery/frontend-react/src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   state = { 
     manager: "",
     players: [],
-    balance: ""
+    balance: "",
+    bettingAmount: ""
   };
 
   async componentDidMount() {
@@ -18,7 +19,32 @@ class App extends Component {
     this.setState({ 
       manager: manager,
       players: players,
-      balance: balance
+      balance: balance,
+      message: ""
+    });
+  }
+
+  // we declare this function as an arrow function so we don't have problems to work with "this" variable
+  // "this" will now point to our component
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    // we assume that the first account of whoever visits the page is the account
+    // he wants to use for betting
+    const newPlayer = accounts[0];
+
+    this.setState({ message: "Waiting for the transaction to be sent to the network..." });
+
+    await lottery.methods.enter().send({
+      from: newPlayer, 
+      value: web3.utils.toWei(this.state.bettingAmount, "ether")
+    });
+
+    const players = await lottery.methods.getPlayers().call();
+    this.setState({ 
+      players: players,
+      message: "The player " + newPlayer + " has entered the lottery!" 
     });
   }
 
@@ -41,6 +67,18 @@ class App extends Component {
           Players:<br/>
           <ul>{playersList}</ul>
         </p>
+        <hr/>
+        <form onSubmit={this.onSubmit}>
+          <h4>How about trying your luck and win some coins?</h4>
+          <div>
+            <label>Amount of ether to enter the lottery:</label>
+            <input value={this.state.bettingAmount} 
+              onChange={event => this.setState({ bettingAmount: event.target.value })}/>
+          </div>
+          <button>Enter</button>
+        </form>
+        <hr/>
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
