36ef7806f774a46e1583febc825a9dbef0144b09
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-15 12:05:26 +0200

    Adding more properties to show on the page

--- Lottery/frontend-react/src/App.js
+++ Lottery/frontend-react/src/App.js
@@ -4,19 +4,35 @@ import web3 from "./web3.config";
 import lottery from "./lottery";
 
 class App extends Component {
-  state = { manager: "" };
+  state = { 
+    manager: "",
+    players: [],
+    balance: ""
+  };
 
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
     
-    this.setState({ manager: manager });
+    this.setState({ 
+      manager: manager,
+      players: players,
+      balance: balance
+    });
   }
 
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by account with the address { this.state.manager }</p>
+        <p>
+          This contract is managed by account with the address { this.state.manager }.
+        </p>
+        <p>
+          There are currently { this.state.players.length } entered the lottery, competing to win
+          { web3.utils.fromWei(this.state.balance, "ether") } ether.
+        </p>
       </div>
     );
   }
