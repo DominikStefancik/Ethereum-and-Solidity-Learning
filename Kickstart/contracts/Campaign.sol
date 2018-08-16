c9015e2dc3b4878e426ac182913dc2ef8b38742b
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-16 12:45:16 +0200

    Rename approvers to contributors; add track of approvals in the Request

--- Kickstart/contracts/Campaign.sol
+++ Kickstart/contracts/Campaign.sol
@@ -9,13 +9,17 @@ contract Campaign {
         address recipient;
         // says if the request is completed and the money sent to the vendor
         bool isCompleted;
+        // number of contributors who approved the given request
+        uint approvalCount;
+        // a mapping of contributors who voted for approving/disapproving the given request
+        mapping(address => bool) approvers;
     }
     
     address public manager;
     uint public minimumContribution;
     
     // IMPORTANT: the keys are NOT stored in the mapping, only values are
-    mapping(address => bool) public approvers;
+    mapping(address => bool) public contributors;
     Request[] public requestList;
     
     modifier restrictedToManager() {
@@ -34,18 +38,23 @@ contract Campaign {
         require(msg.value >= minimumContribution);
         
         // IMPORTANT: the addresses are NOT stored in the mapping, only bool values are
-        approvers[msg.sender] = true;
+        contributors[msg.sender] = true;
     }
     
     function createRequest(string description, uint value, address recipient) 
         public restrictedToManager {
         // whenever we create a new object it is located in memory
         // that's why we need to insert the keyword "memory" when we declare a new variable
+        //
+        // whenever we create a new instance of a struct, we need to provide values for all its 
+        // VALUE type properties 
+        // REFERENCE types DON'T have to be provided (here the property of mapping doesn't have to be provided)
         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
-            isCompleted: false
+            isCompleted: false,
+            approvalCount: 0
         });
         
         requestList.push(newRequest);
