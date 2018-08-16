45ed161323ba1c06c7dc0addf1ef6e9eaec28468
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-17 00:51:04 +0200

    Create a campaign and get its address

--- Kickstart/test/Campaign.test.js
+++ Kickstart/test/Campaign.test.js
@@ -7,14 +7,34 @@ const compiledCampaignFactory = require("../ethereum/build/CampaignFactory.json"
 const compiledCampaign = require("../ethereum/build/Campaign.json");
 
 let accounts;
+let testAccount;
 let factoryContract;
 let campaignAddress;
-let campaign;
+let campaignContract;
 
 beforeEach(async () => {
   accounts = web3.eth.getAccounts();
+  testAccount = accounts[0];
+
+  // we use this way of creating a new contract Javascript object when we want to 
+  // deploy the contract
   factoryContract = await new web3.eth.Contract(JSON.parse(compiledCampaignFactory.interface))
     .deploy({ data: compiledCampaignFactory.bytecode })
-    .send({ from: accounts[0], gas: "1000000" });
+    .send({ from: testAccount, gas: "1000000" });
 
+  // the send method will not return anything, that's why we need to get the new campaing differently  
+  await factoryContract.methods.createCampaign("1000").send({
+    from: testAccount, 
+    gas: "1000000"
+  });
+
+  const deployedCampaigns = await factoryContract.methods.getDeployedCampaigns().call();
+  campaignAddress = deployedCampaigns[0];
+
+  // we use this way of creating a new contract Javascript object when the contract is already
+  // deployed and we have its address
+  campaignAddress = await new web3.eth.Contract(
+    JSON.parse(compiledCampaign.interface), 
+    campaignAddress
+  );
 });
