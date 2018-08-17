a812e7d00ca71df74ed12083b93407b722d97b0f
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-17 12:01:29 +0200

    Create a deployment script

---
+++ Kickstart/ethereum/deploy.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const compiledCampaignFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "rail shove napkin hand cool grass ordinary behave pilot agent reason mystery",
+  "https://rinkeby.infura.io/v3/48563856dfcb4e068564732cc8d4ee38"
+);
+
+const web3 = new Web3(provider);
+
+let deploymentAccount;
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  deploymentAccount = accounts[0];
+
+  console.log("Attempting to deploy from account", deploymentAccount);
+
+  const deployedContract = await new web3.eth.Contract(JSON.parse(compiledCampaignFactory.interface))
+    .deploy({ data: "0x" + compiledCampaignFactory.bytecode })
+    .send({ from: deploymentAccount, gas: "1000000" });
+
+  console.log("ABI of the deployed contract:\n", compiledCampaignFactory.interface);  
+  console.log("Contract deployed to address", deployedContract.options.address);
+}
+
+deploy();
