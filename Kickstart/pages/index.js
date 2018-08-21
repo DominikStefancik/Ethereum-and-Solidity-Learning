5dbae2ab0a046be7a6a3f7cbab03c9692e7bf8bc
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-21 22:55:28 +0200

    Rendering of the campaign list

--- Kickstart/pages/index.js
+++ Kickstart/pages/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
+
 import campaignFactory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
@@ -9,9 +11,24 @@ class CampaignIndex extends Component {
     return { campaigns: campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View campaign</a>,
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />
+  }
+
   render() {
     return (
-      <div>{this.props.campaigns}</div>
+      <div>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css" />
+        <div>{this.renderCampaigns()}</div>
+      </div>
     );
   }
 }
