c25636ccd780db818c6f3ace662bc0dc8adb7e46
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-21 23:51:08 +0200

    Create a header component

---
+++ Kickstart/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+export default (props) => {
+  return (
+    <Menu>
+        <Menu.Item name="kickstarter">
+          Kickstarter
+        </Menu.Item>
+
+        <Menu.Menu position="right">
+          <Menu.Item name="campaigns">
+            Campaigns
+          </Menu.Item>
+          <Menu.Item name="plus">
+            +
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+  );
+}
