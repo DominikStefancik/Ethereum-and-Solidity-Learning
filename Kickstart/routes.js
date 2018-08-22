5fce0a059d23bfe3c6e58e5f408a1c1bb5650ec6
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-22 14:36:56 +0200

    Setup Next server so it can handle dynamic routes

---
+++ Kickstart/routes.js
@@ -0,0 +1,5 @@
+// "next-routes" enables us to define dynamic routes which contain tokens/parameters in their URL
+// "next-routes" exports a function which we need to immediately call
+const routes = require("next-routes")();
+
+module.exports = routes;
