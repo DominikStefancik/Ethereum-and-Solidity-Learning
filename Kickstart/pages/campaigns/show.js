091d51111569b6ca403ececb9453c19e793865c9
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-23 15:27:32 +0200

    Render the summary info about a campaign

--- Kickstart/pages/campaigns/show.js
+++ Kickstart/pages/campaigns/show.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 
 import Layout from "../../components/Layout";
 import campaignProvider from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
+
+// the keys are "indexed" according to the order specified in the "getSummary" method
+// in the Campaign contract 
+const MINIMUM_CONTRIBUTION_KEY = "0";
+const CAMPAIGN_BALANCE_KEY = "1";
+const REQUESTS_COUNT_KEY = "2";
+const CONTRIBUTORS_COUNT_KEY = "3";
+const MANAGER_KEY = "4";
 
 class ShowCampaign extends Component {
   // this method is Next.js specific and is called before a component is created
@@ -12,13 +22,57 @@ class ShowCampaign extends Component {
     const summary = await campaign.methods.getSummary().call();
 
     // this object is returned as "props" object when the component is rendered 
-    return {};
+    return {
+      minimumContribution: summary[MINIMUM_CONTRIBUTION_KEY],
+      campaignBalance: summary[CAMPAIGN_BALANCE_KEY],
+      requestsCount: summary[REQUESTS_COUNT_KEY],
+      contributorsCount: summary[CONTRIBUTORS_COUNT_KEY],
+      manager: summary[MANAGER_KEY]
+    };
+  }
+
+  renderCards() {
+    const items = [
+      {
+        header: this.props.manager,
+        description: "Manager has created this campaign and can create a request to withdraw money",
+        meta: "Address of manager",
+        style: {
+          overflowWrap: "break-word"
+        }
+      },
+      {
+        header: this.props.minimumContribution,
+        description: "A minimum amount of money in order to contribute to the campaign to become " + 
+          "a contributor and a request approver",
+        meta: "Minimum Contribution (in wei)",
+      },
+      {
+        header: this.props.requestsCount,
+        description: "Requests try to withdraw money from the campaign contract. " + 
+          "Requests must be approved by at least 51% of all contributors in order to be processed",
+        meta: "Number of requests",
+      },
+      {
+        header: this.props.contributorsCount,
+        description: "A number of people who have already donated (contributed) to this campaign",
+        meta: "Number of contributors",
+      },
+      {
+        header: web3.utils.fromWei(this.props.campaignBalance, "ether"),
+        description: "The balance is how much money has this campaign left to spend",
+        meta: "Campaign Balance (in ether)",
+      }
+    ];
+
+    return <Card.Group items={items} />;
   }
 
   render() {
     return (
       <Layout>
         <h3>Campaign Details</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
