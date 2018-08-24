5456777e2545f00bd8b25bc9f353bd202ba52190
LAPTOP-QVVQCVTI\Dominik <dominikstefancik@seznam.cz>
2018-08-24 23:53:04 +0200

    Get all requests of a campaign; create a table header

--- Kickstart/pages/campaigns/requests/list.js
+++ Kickstart/pages/campaigns/requests/list.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Icon, Label, Menu, Table } from 'semantic-ui-react';
 
 import Layout from "../../../components/Layout";
 import { Link } from "../../../routes";
@@ -11,11 +11,28 @@ class RequestList extends Component {
     const { campaignAddress } = props.query;
     const campaign = campaignProvider(campaignAddress);
 
+    const requestsCount = await campaign.methods.getRequestCount().call();
+
+    // Solidity does NOT support retrieving a whole array which items are of a complex type
+    // Request in our Campaign contract is of the type Struct which is a complex type
+    // we cannot retrieve our array of requests in one call, so we need to iterate over the requests
+    // and retrieve each of them one by one
+    const requests = await Promise.all(
+      // Promise.all() will acummulate all asynchronous calls which retrieve request at the given index
+      Array(requestsCount).fill().map((element, index) => {
+        return campaign.methods.requestList(index).call();
+      })
+    );
+
+    console.log(requests);
+
     // this statement is equivalent to "return { campaignAddress: campaignAddress }"
-    return { campaignAddress };
+    return { campaignAddress, requests, requestsCount };
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body} = Table;
+
     return (
       <Layout>
         <h3>Request List</h3>
@@ -24,6 +41,20 @@ class RequestList extends Component {
             <Button primary>Add Request</Button>
           </a>
         </Link>
+
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalise</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
 
       </Layout>
     );
